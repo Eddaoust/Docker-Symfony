6ff86a38f629c1a5492d5677afada83e2fb29b7e
Eddaoust <contact@eddaoust.com>
2025-06-18 10:21:18 +0200

    Remove obsolete PHP configuration files and Symfony CLI steps
    Deleted unused PHP configuration files, including FPM, OpCache, and JIT settings, along with Symfony CLI installation and Git config steps from PHP Dockerfiles. Updated Docker-related projects to streamline configurations and removed redundant dependencies.

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
+	# Install the project the first time PHP is started
+	# After the installation, the following block can be deleted
+	if [ ! -f composer.json ]; then
+		rm -Rf tmp/
+		composer create-project "symfony/skeleton $SYMFONY_VERSION" tmp --stability="$STABILITY" --prefer-dist --no-progress --no-interaction --no-install
+
+		cd tmp
+		cp -Rp . ..
+		cd -
+		rm -Rf tmp/
+  fi
+fi
+
+exec docker-php-entrypoint "$@"
